be28e80c039a0029d81718290e22fa9da2dc57c4
Dede Dodu <sebnat94@hotmail.com>
2025-04-22 16:54:24 +0200

    loco_controller: Implement HTTP and backend servers
    This is a first stab at implementing the HTTP server which receives HTTP
    requests and how they get translated into calls to the backend server.
    The same backend server listens on a different port for incoming
    connections from the locos in order to register their active TCP
    stream.
    
    Signed-off-by: Dede Dodu <sebnat94@hotmail.com>

--- loco_controller/src/main.rs
+++ loco_controller/src/main.rs
@@ -1,3 +1,479 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    App, HttpResponse, HttpServer, Responder, body::BoxBody, get, http::StatusCode, post, web,
+};
+use bincode::{
+    Decode, Encode,
+    config::{Configuration, Fixint, LittleEndian, NoLimit},
+    decode_from_std_read, encode_to_vec,
+    error::{DecodeError, EncodeError},
+};
+use clap::Parser;
+use log::{debug, error};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    fmt,
+    io::{self, Read, Write},
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
+};
+use thiserror::Error;
+
+const BACKEND_PROTOCOL_MAGIC_NUMBER: u8 = 0xab;
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Error binding listener {0}")]
+    BindListener(#[source] io::Error),
+    #[error("Error decoding from TCP stream: {0}")]
+    DecodeFromStream(#[source] DecodeError),
+    #[error("Error encoding to vec: {0}")]
+    EncodeToVec(#[source] EncodeError),
+    #[error("Error running HTTP server {0}")]
+    HttpServer(#[source] io::Error),
+    #[error("Invalid backend protocol magic number {0}")]
+    InvalidBackendProtocolMagicNumber(u8),
+    #[error("Loco {0} not connected")]
+    LocoNotConnected(LocoId),
+    #[error("Error reading from TCP stream {0}")]
+    ReadTcpStream(#[source] io::Error),
+    #[error("Unknown direction {0}")]
+    UnknownDirection(u8),
+    #[error("Unknown loco identifier {0}")]
+    UnknownLocoId(u8),
+    #[error("Unknown operation {0}")]
+    UnknownOperation(u8),
+    #[error("Unknown speed {0}")]
+    UnknownSpeed(u8),
+    #[error("Unsupported operation {0}")]
+    UnsupportedOperation(Operation),
+    #[error("Error writing to TCP stream {0}")]
+    WriteTcpStream(#[source] io::Error),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Copy, Clone, Debug, Deserialize, Eq, Hash, PartialEq)]
+#[serde(rename_all = "lowercase")]
+enum LocoId {
+    Loco1,
+    Loco2,
+}
+
+impl TryFrom<u8> for LocoId {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self> {
+        Ok(match value {
+            1 => LocoId::Loco1,
+            2 => LocoId::Loco2,
+            _ => return Err(Error::UnknownLocoId(value)),
+        })
+    }
+}
+
+impl Into<u8> for LocoId {
+    fn into(self) -> u8 {
+        match self {
+            LocoId::Loco1 => 1,
+            LocoId::Loco2 => 2,
+        }
+    }
+}
+
+impl fmt::Display for LocoId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let id = match *self {
+            LocoId::Loco1 => "Loco1",
+            LocoId::Loco2 => "Loco2",
+        };
+        write!(f, "{}", id)
+    }
+}
+
+#[derive(Copy, Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+enum Direction {
+    #[default]
+    Forward,
+    Backward,
+}
+
+impl TryFrom<u8> for Direction {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self> {
+        Ok(match value {
+            1 => Direction::Forward,
+            2 => Direction::Backward,
+            _ => return Err(Error::UnknownDirection(value)),
+        })
+    }
+}
+
+impl Into<u8> for Direction {
+    fn into(self) -> u8 {
+        match self {
+            Direction::Forward => 1,
+            Direction::Backward => 2,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+enum Speed {
+    #[default]
+    Stop,
+    Slow,
+    Normal,
+    Fast,
+}
+
+impl TryFrom<u8> for Speed {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self> {
+        Ok(match value {
+            0 => Speed::Stop,
+            1 => Speed::Slow,
+            2 => Speed::Normal,
+            3 => Speed::Fast,
+            _ => return Err(Error::UnknownSpeed(value)),
+        })
+    }
+}
+
+impl Into<u8> for Speed {
+    fn into(self) -> u8 {
+        match self {
+            Speed::Stop => 0,
+            Speed::Slow => 1,
+            Speed::Normal => 2,
+            Speed::Fast => 3,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Deserialize)]
+struct ControlLoco {
+    loco_id: LocoId,
+    direction: Direction,
+    speed: Speed,
+}
+
+#[derive(Decode, Copy, Clone, Debug)]
+enum Operation {
+    Connect,
+    ControlLoco,
+    LocoStatus,
+}
+
+impl TryFrom<u8> for Operation {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self> {
+        Ok(match value {
+            1 => Operation::Connect,
+            2 => Operation::ControlLoco,
+            3 => Operation::LocoStatus,
+            _ => return Err(Error::UnknownOperation(value)),
+        })
+    }
+}
+
+impl Into<u8> for Operation {
+    fn into(self) -> u8 {
+        match self {
+            Operation::Connect => 1,
+            Operation::ControlLoco => 2,
+            Operation::LocoStatus => 3,
+        }
+    }
+}
+
+impl fmt::Display for Operation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let op = match *self {
+            Operation::Connect => "Connect",
+            Operation::ControlLoco => "ControlLoco",
+            Operation::LocoStatus => "LocoStatus",
+        };
+        write!(f, "{}", op)
+    }
+}
+
+#[derive(Decode, Copy, Clone, Debug)]
+struct ConnectPayload {
+    loco_id: u8,
+}
+
+#[derive(Encode, Copy, Clone, Debug)]
+struct ControlLocoPayload {
+    direction: u8,
+    speed: u8,
+}
+
+#[derive(Decode, Copy, Clone, Debug)]
+struct LocoStatusResponse {
+    direction: u8,
+    speed: u8,
+}
+
+#[derive(Encode, Decode, Copy, Clone, Debug)]
+struct Header {
+    magic: u8,
+    operation: u8,
+    payload_len: u8,
+}
+
+#[derive(Serialize)]
+struct LocoStatus {
+    direction: Direction,
+    speed: Speed,
+}
+
+struct LocoInfo {
+    stream: TcpStream,
+}
+
+struct Backend {
+    bincode_cfg: Configuration<LittleEndian, Fixint, NoLimit>,
+    loco_info: HashMap<LocoId, LocoInfo>,
+}
+
+impl Backend {
+    fn new() -> Self {
+        debug!("Backend::new()");
+
+        let bincode_cfg = bincode::config::legacy();
+        let loco_info = HashMap::new();
+
+        Backend {
+            bincode_cfg,
+            loco_info,
+        }
+    }
+
+    fn handle_op_connect(&mut self, mut stream: TcpStream) -> Result<()> {
+        debug!("Backend::handle_op_connect()");
+
+        // Retrieve payload
+        let payload: ConnectPayload = decode_from_std_read(&mut stream, self.bincode_cfg.clone())
+            .map_err(Error::DecodeFromStream)?;
+        let loco_id = LocoId::try_from(payload.loco_id)?;
+        debug!("Backend::handle_op_connect(): LocoId {:?}", loco_id);
+
+        self.loco_info.insert(loco_id, LocoInfo { stream });
+
+        Ok(())
+    }
+
+    fn handle_connection(&mut self, mut stream: TcpStream) -> Result<()> {
+        debug!("Backend::handle_connection()");
+
+        // Retrieve header
+        let header: Header = decode_from_std_read(&mut stream, self.bincode_cfg.clone())
+            .map_err(Error::DecodeFromStream)?;
+
+        debug!("Backend::handle_connection(): {:?}", header);
+
+        if header.magic != BACKEND_PROTOCOL_MAGIC_NUMBER {
+            return Err(Error::InvalidBackendProtocolMagicNumber(header.magic));
+        }
+
+        let mut payload = vec![0u8; header.payload_len as usize];
+        stream
+            .read_exact(payload.as_mut_slice())
+            .map_err(Error::ReadTcpStream)?;
+
+        let op = Operation::try_from(header.operation)?;
+        debug!("Backend::handle_connection(): Operation {:?}", op);
+
+        match op {
+            Operation::Connect => self.handle_op_connect(stream)?,
+            Operation::ControlLoco | Operation::LocoStatus => {
+                return Err(Error::UnsupportedOperation(op));
+            }
+        }
+
+        Ok(())
+    }
+
+    fn control_loco(&mut self, loco_id: LocoId, direction: Direction, speed: Speed) -> Result<()> {
+        debug!(
+            "Backend::control_loco(): loco_id {:?}, direction {:?}, speed {:?}",
+            loco_id, direction, speed
+        );
+
+        let loco_info = self
+            .loco_info
+            .get_mut(&loco_id)
+            .ok_or(Error::LocoNotConnected(loco_id))?;
+
+        let mut payload = encode_to_vec(
+            ControlLocoPayload {
+                direction: direction.into(),
+                speed: speed.into(),
+            },
+            self.bincode_cfg.clone(),
+        )
+        .map_err(Error::EncodeToVec)?;
+
+        let mut message = encode_to_vec(
+            Header {
+                magic: BACKEND_PROTOCOL_MAGIC_NUMBER,
+                operation: Operation::ControlLoco.into(),
+                payload_len: payload.len() as u8,
+            },
+            self.bincode_cfg.clone(),
+        )
+        .map_err(Error::EncodeToVec)?;
+
+        message.append(&mut payload);
+
+        loco_info
+            .stream
+            .write_all(message.as_slice())
+            .map_err(Error::WriteTcpStream)?;
+
+        Ok(())
+    }
+
+    fn loco_status(&mut self, loco_id: LocoId) -> Result<LocoStatus> {
+        debug!("Backend::loco_status(): loco_id {:?}", loco_id);
+
+        let loco_info = self
+            .loco_info
+            .get_mut(&loco_id)
+            .ok_or(Error::LocoNotConnected(loco_id))?;
+
+        let message = encode_to_vec(
+            Header {
+                magic: BACKEND_PROTOCOL_MAGIC_NUMBER,
+                operation: Operation::LocoStatus.into(),
+                payload_len: 0,
+            },
+            self.bincode_cfg.clone(),
+        )
+        .map_err(Error::EncodeToVec)?;
+
+        loco_info
+            .stream
+            .write_all(message.as_slice())
+            .map_err(Error::WriteTcpStream)?;
+
+        let resp: LocoStatusResponse =
+            decode_from_std_read(&mut loco_info.stream, self.bincode_cfg.clone())
+                .map_err(Error::DecodeFromStream)?;
+
+        let status = LocoStatus {
+            direction: Direction::try_from(resp.direction)?,
+            speed: Speed::try_from(resp.speed)?,
+        };
+
+        Ok(status)
+    }
+}
+
+#[get("/")]
+async fn index(_data: web::Data<Arc<Mutex<Backend>>>) -> impl Responder {
+    HttpResponse::Ok().body("Loco controller running!")
+}
+
+#[get("/loco_status/{loco_id}")]
+async fn loco_status(
+    path: web::Path<LocoId>,
+    data: web::Data<Arc<Mutex<Backend>>>,
+) -> impl Responder {
+    let loco_id = path.into_inner();
+
+    match data.lock().unwrap().loco_status(loco_id) {
+        Ok(status) => HttpResponse::Ok().json(status),
+        Err(e) => {
+            error!("{}\n", e);
+            return HttpResponse::with_body(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                BoxBody::new(format!("{}", e)),
+            );
+        }
+    }
+}
+
+#[post("/control_loco")]
+async fn control_loco(
+    form: web::Json<ControlLoco>,
+    data: web::Data<Arc<Mutex<Backend>>>,
+) -> impl Responder {
+    if let Err(e) = data
+        .lock()
+        .unwrap()
+        .control_loco(form.loco_id, form.direction, form.speed)
+    {
+        error!("{}\n", e);
+        return HttpResponse::with_body(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            BoxBody::new(format!("{}", e)),
+        );
+    }
+
+    HttpResponse::Ok().body(format!(
+        "Move {:?} loco {:?} at {:?} speed",
+        form.direction, form.loco_id, form.speed
+    ))
+}
+
+#[actix_web::main]
+async fn http_main(port: u16, backend: Arc<Mutex<Backend>>) -> std::io::Result<()> {
+    debug!("http_main(): Waiting for incoming connection...");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(backend.clone()))
+            .service(index)
+            .service(loco_status)
+            .service(control_loco)
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
+}
+
+fn backend_main(port: u16, backend: Arc<Mutex<Backend>>) -> Result<()> {
+    let listener = TcpListener::bind(("localhost", port)).map_err(Error::BindListener)?;
+
+    loop {
+        debug!("backend_main(): Waiting for incoming connection...");
+        let (stream, _) = listener.accept().map_err(Error::BindListener)?;
+        debug!("main(): Connected\n");
+        if let Err(e) = backend.lock().unwrap().handle_connection(stream) {
+            error!("{}\n", e);
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value_t = 8080)]
+    http_port: u16,
+    #[arg(long, default_value_t = 8004)]
+    backend_port: u16,
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let args = Args::parse();
+
+    // Initialize backend
+    let backend = Arc::new(Mutex::new(Backend::new()));
+    let shared_backend = backend.clone();
+
+    // Start backend server, waiting for incoming connections from locos
+    thread::spawn(move || backend_main(args.backend_port, backend));
+
+    http_main(args.http_port, shared_backend).map_err(Error::HttpServer)?;
+
+    Ok(())
 }
