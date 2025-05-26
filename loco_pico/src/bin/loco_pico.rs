e95a5b34a65e9e657671cbdffba8d2b258660b51
Dede Dodu <sebnat94@hotmail.com>
2025-05-26 21:54:51 +0200

    cyw43-firmware: Move cyw43 firmwares to root directory
    This allows firmwares for the cyw43 module to be reused across all
    different crates, rather than duplicating them everytime.
    
    Signed-off-by: Dede Dodu <sebnat94@hotmail.com>

--- loco_pico/src/bin/loco_pico.rs
+++ loco_pico/src/bin/loco_pico.rs
@@ -71,8 +71,8 @@ async fn main(spawner: Spawner) {
     }
     log::info!("Hello LocoPico!");
 
-    let fw = include_bytes!("../../cyw43-firmware/43439A0.bin");
-    let clm = include_bytes!("../../cyw43-firmware/43439A0_clm.bin");
+    let fw = include_bytes!("../../../cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../../../cyw43-firmware/43439A0_clm.bin");
 
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
